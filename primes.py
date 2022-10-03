580afff01b8a3dc57adc62270a8e68bfb6bf5c86
Rand Albahusain <114617321+RandAbdull@users.noreply.github.com>
2022-10-03 16:22:19 +0100

    Raise a ValueError when input is less than 1

--- primes.py
+++ primes.py
@@ -1,6 +1,33 @@
-"""List of prime numbers generator."""
-"""ENTER YOUR SOLUTION HERE!"""
+potentialprime = 2
 
+# check if a number is prime
+def primetest(potentialprime):
+    divisor = 2
+    
+    while divisor <= potentialprime:
+        if potentialprime == 2:
+            return True
+        elif potentialprime % divisor == 0:
+            return False
+        while potentialprime % divisor != 0:
+            if potentialprime - divisor > 1:
+                divisor += 1
+            else:
+                return True
+            
 def primes(number_of_primes):
-    list = []
-    return list
+    
+    if number_of_primes <= 0:
+        raise ValueError("Please enter an integer greater than 0!")
+        
+    count = 0
+    primesList = []
+    
+    # generate prime numbers until count reaches the user input number            
+    while count < inputNum:
+        if primetest(potentialprime) == True:
+            # add the prime number to the list
+            primesList.append(potentialprime)
+            count += 1
+        potentialprime += 1
+    return primesList
